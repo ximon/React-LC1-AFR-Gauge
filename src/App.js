9979598f3b6a3f1678bed3832ed79171d2bdc58e
Simon Keen <simon.crx@gmail.com>
2022-06-17 16:30:02 +0100

    Initial work on showing a gauge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import RadialGauge from 'react-canvas-gauges/dist/RadialGauge';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RadialGauge
+        units=''
+        title='AFR'
+        value={14.7}
+        height={300}
+        minValue={8}
+        maxValue={20}
+        majorTicks={['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']}
+        minorTicks={5}
+        highlights={[
+          { "from": 8, "to": 9, "color": "rgba(255,30,0,.25)" },
+          { "from": 9, "to": 10.5, "color": "rgba(255,30,0,.25)" },
+          { "from": 10.5, "to": 12, "color": "rgba(255,255,0,.25)" },
+          { "from": 12, "to": 14, "color": "rgba(0,255,0,.15)" },
+          { "from": 14, "to": 20, "color": "rgba(255,30,0,.25)" },
+        ]}
+      />
+
     </div>
   );
 }
