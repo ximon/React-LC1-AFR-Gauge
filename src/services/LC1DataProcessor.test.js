10e8ab43a61b6458c86c1cdddb0e593e20440b66
Simon Keen <simon.crx@gmail.com>
2022-08-15 23:13:40 +0100

    Added more tests

--- src/services/LC1DataProcessor.test.js
+++ src/services/LC1DataProcessor.test.js
@@ -1,21 +1,53 @@
 import { expect } from '@esm-bundle/chai';
-
 import { processData, addEventListener } from './LC1DataProcessor'
+import { States } from './LC1Data'
 
 
-it('should decode the state as normal and the afr as 14.7', () => {
+describe('should decode the data', () => {
+
+    let state = {}
+
     addEventListener('update', onUpdate)
 
-    function onUpdate({state, prevState}) {
-        console.log(state)
-        expect(state.afr).to.equal(14.7)
-        expect(state.stateId).to.equal(0)
+    beforeEach(() => {
+        state = {};
+    })
+
+    function onUpdate({state: newState}) {
+        console.log(newState)
+        state = newState
     }
 
-    processData(0xb2)
-    processData(0x82)
-    processData(0x43)
-    processData(0x13)
-    processData(0x03)
-    processData(0x74)
+    it('the state should be normal with an afr of 14.7', () => {
+        processData(0xb2)
+        processData(0x82)
+        processData(0x43)
+        processData(0x13)
+        processData(0x03)
+        processData(0x74)
+
+        expect(state.stateId).equal(States.Normal)
+        expect(state.afr).equal(14.7)
+    })
+
+    it('the state should be warmup with a countdown of 66%', () => {
+        processData(0xb2);
+        processData(0x82);
+        processData(0x53)
+        processData(0x13)
+        processData(0x05)
+        processData(0x14)
+
+        expect(state.stateId).equal(States.Warmup)
+        expect(state.warmup).equal(66)
+    })
+
+    it('the state shoule be error with a code of 9', () => {
+        processData(0xb2)
+        processData(0x82)
+        processData(0x5b)
+        processData(0x13)
+        processData(0x00)
+        processData(0x09)
+    })
 })
